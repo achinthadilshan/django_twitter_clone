01e1a5b03de467d5f56bb27c746d471360cf7260
ACHii <>
2023-09-29 02:12:14 +0530

    update

--- main/views.py
+++ main/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .models import *
 
 # Create your views here.
@@ -11,6 +11,20 @@ def home(request):
 
 
 def profile_list(request):
-    profiles = Profile.objects.exclude(user=request.user)
-    context = {"profiles": profiles}
-    return render(request, 'profile_list.html', context)
+    if request.user.is_authenticated:
+        profiles = Profile.objects.exclude(user=request.user)
+        context = {"profiles": profiles}
+        return render(request, 'profile_list.html', context)
+    else:
+        messages.success(request, ('You must be logged in to view this page!'))
+        return redirect('home')
+
+
+def profile(request, pk):
+    if request.user.is_authenticated:
+        profile = Profile.objects.get(user_id=pk)
+        context = {"profile": profile}
+        return render(request, 'profile.html', context)
+    else:
+        messages.success(request, ('You must be logged in to view this page!'))
+        return redirect('home')
