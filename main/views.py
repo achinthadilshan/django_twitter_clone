e6c0631e40384862006f6f1ba5c1b72fc4ea3c86
ACHii <>
2023-10-01 18:29:03 +0530

    update

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
 from .models import *
 from .forms import TweetForm
 
@@ -61,3 +62,29 @@ def profile(request, pk):
     else:
         messages.success(request, ('You must be logged in to view this page!'))
         return redirect('home')
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, ('You have been logged in!'))
+            return redirect('home')
+        else:
+            messages.success(request, ('Couldn\'t find an user!'))
+            return redirect('login')
+    else:
+        if request.user.is_authenticated:
+            messages.success(request, ('You\'re already logged in!'))
+            return redirect('home')
+        else:
+            return render(request, 'login.html')
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, ('You have been logged out!'))
+    return redirect('home')
